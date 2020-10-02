64ad65a86467c10447907efcda9513c8a9c24d71
Aleksander Gąsior <a.gasior@azagroup.eu>
2020-10-02 14:57:07 +0200

    Updates

---
+++ precache-manifest.aec243def42e96084f66a6c74010c9b0.js
@@ -0,0 +1,122 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f74b985e5f19b98ce03305db86da8de6",
+    "url": "/restaurant-page/index.html"
+  },
+  {
+    "revision": "56cc1923021505133a80",
+    "url": "/restaurant-page/static/css/2.4eaa4f3d.chunk.css"
+  },
+  {
+    "revision": "acefe37d9167f5cea60d",
+    "url": "/restaurant-page/static/css/main.349695ca.chunk.css"
+  },
+  {
+    "revision": "56cc1923021505133a80",
+    "url": "/restaurant-page/static/js/2.22cf26d8.chunk.js"
+  },
+  {
+    "revision": "2069192f75c6f3fb6247231eea52f248",
+    "url": "/restaurant-page/static/js/2.22cf26d8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "acefe37d9167f5cea60d",
+    "url": "/restaurant-page/static/js/main.c3b59f9e.chunk.js"
+  },
+  {
+    "revision": "7d34f849cd98231e5d72",
+    "url": "/restaurant-page/static/js/runtime-main.53ebd7cc.js"
+  },
+  {
+    "revision": "b1ef6a4a3a0adbd950a91a9ac0d7a71a",
+    "url": "/restaurant-page/static/media/bar.b1ef6a4a.jpg"
+  },
+  {
+    "revision": "92dfca4d38243640a180935e773315b5",
+    "url": "/restaurant-page/static/media/cafe.92dfca4d.jpg"
+  },
+  {
+    "revision": "ff0b917f754c89e3284030d53e9fb1f7",
+    "url": "/restaurant-page/static/media/chef.ff0b917f.jpg"
+  },
+  {
+    "revision": "303844d36ec6e139b0c7ca1335829f5c",
+    "url": "/restaurant-page/static/media/chefs.303844d3.jpg"
+  },
+  {
+    "revision": "6b3464e0e9ae65a71c2b77e415179377",
+    "url": "/restaurant-page/static/media/dessert1.6b3464e0.jpg"
+  },
+  {
+    "revision": "aae5f20a073fe56628bc47692cbc0348",
+    "url": "/restaurant-page/static/media/dessert2.aae5f20a.jpg"
+  },
+  {
+    "revision": "1123a51b5b92d28a58b043bee196f3c2",
+    "url": "/restaurant-page/static/media/dessert3.1123a51b.jpg"
+  },
+  {
+    "revision": "787be3dec3e1c3e504a702cd1c09fac4",
+    "url": "/restaurant-page/static/media/dish-about.787be3de.jpg"
+  },
+  {
+    "revision": "9883536fe1eebf11950f2c874fbee4b0",
+    "url": "/restaurant-page/static/media/kitchen.9883536f.jpg"
+  },
+  {
+    "revision": "78dca1fac78df5df81d3aea57c056071",
+    "url": "/restaurant-page/static/media/main1.78dca1fa.jpg"
+  },
+  {
+    "revision": "6deaa65499724e9c3bbe8646089cf492",
+    "url": "/restaurant-page/static/media/main2.6deaa654.jpg"
+  },
+  {
+    "revision": "1f5b65e5715879d28e490b8bb07d632d",
+    "url": "/restaurant-page/static/media/main3.1f5b65e5.jpg"
+  },
+  {
+    "revision": "b855d4f2ede7eaccdfd18af6478df297",
+    "url": "/restaurant-page/static/media/reservation.b855d4f2.jpg"
+  },
+  {
+    "revision": "5e40438b7012ca5c3b089fc4b41fba3f",
+    "url": "/restaurant-page/static/media/restaurant.5e40438b.jpg"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/restaurant-page/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/restaurant-page/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/restaurant-page/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/restaurant-page/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "9830df8122ff72d723973d969d510c5d",
+    "url": "/restaurant-page/static/media/starter1.9830df81.jpg"
+  },
+  {
+    "revision": "47cb58bbb3a1073c39d2c44860cad038",
+    "url": "/restaurant-page/static/media/starter2.47cb58bb.jpg"
+  },
+  {
+    "revision": "09341121177d034c00df76ade1b49872",
+    "url": "/restaurant-page/static/media/starter3.09341121.jpg"
+  },
+  {
+    "revision": "35def83dd782dd42ab91c86af1ef36ea",
+    "url": "/restaurant-page/static/media/team.35def83d.jpg"
+  },
+  {
+    "revision": "b29dfeadb30b5cd5beb61e10f30594d2",
+    "url": "/restaurant-page/static/media/tiramisu.b29dfead.jpg"
+  }
+]);
